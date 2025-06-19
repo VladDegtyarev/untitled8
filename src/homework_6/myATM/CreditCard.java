658ffce988630038a184691f8065ae0e6dc11b50
VladDegtyarev <vladdegtyarev3@gmail.com>
2025-06-19 22:42:52 +0300

    Initial commit

---
+++ src/homework_6/myATM/CreditCard.java
@@ -0,0 +1,34 @@
+package homework_6.myATM;
+
+
+
+
+public class CreditCard {
+    private String nameOwner;
+    private long creditCardNumber;
+    private long creditCardBalance;
+
+
+    public CreditCard(String nameOwner,long creditCardNumber, long creditCardValance) {
+        this.nameOwner = nameOwner;
+        this.creditCardNumber = creditCardNumber;
+        this.creditCardBalance = creditCardValance;
+    }
+
+    public String getDebitCardInformation() {
+        return "Your name are : " + nameOwner+"\n"+
+                "Debit Card Number is: " + creditCardNumber + "\n" +
+                "Debit Card balance is:  " + creditCardBalance + "\n" ;
+
+
+    }
+    public void addMoney(long money){
+        this.creditCardBalance += money;
+    }
+
+    public void takeMoney(long money){
+        this.creditCardBalance -= money;
+    }
+
+
+}
---
+++ src/homework_6/myATM/MyHomework.java
@@ -0,0 +1,41 @@
+package homework_6.myATM;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MyHomework {
+    public static void main(String[] args) {
+
+        CreditCard();
+    }
+    public static Scanner getInput() {
+        return new Scanner(System.in);
+    }
+    public static long random(){
+        Random random = new Random();
+        long numCard = random.nextLong(623242455255l,9232323232324l);
+        return numCard ;
+    }
+    public static void CreditCard(){
+        CreditCard firstPerson = new CreditCard("Vlad",random(), (long) (Math.random()*10000));
+        System.out.printf(firstPerson.getDebitCardInformation());
+        System.out.print("Please, enter how much do you want to put in : ");
+        firstPerson.addMoney(getInput().nextLong());
+        System.out.println("****************************************************************");
+
+        CreditCard secondPerson = new CreditCard("Lisa", random(), (long) (Math.random()*10000));
+        System.out.printf(secondPerson.getDebitCardInformation());
+        System.out.print("Please, enter how much do you want to put in : ");
+        secondPerson.addMoney(getInput().nextLong());
+        System.out.println("****************************************************************");
+
+        CreditCard thirdPerson = new CreditCard("Dima", random(), (long) (Math.random()*10000));
+        System.out.printf(thirdPerson.getDebitCardInformation());
+        System.out.print("Please, enter how much do you want to withdraw :");
+        thirdPerson.takeMoney(getInput().nextLong());
+        System.out.println("*****************************************************************");
+        System.out.println(firstPerson.getDebitCardInformation());
+        System.out.println(secondPerson.getDebitCardInformation());
+        System.out.printf(thirdPerson.getDebitCardInformation());
+    }
+}
